6a9bf1d676b563d365487b6fafe1e59c9aedf7e0
Leon Hsu <leonhsu95@gmail.com>
2021-08-26 22:24:29 +1000

    Updates

--- scripts/animate.js
+++ scripts/animate.js
@@ -31,6 +31,16 @@ gsap.to(".skills-box",{
 
 timeline = gsap.timeline({scrollTrigger: "#portfolio"});
 timeline
+.from("#project0", { 
+    y: "-10%",
+    opacity: 0, 
+    delay: 1,
+})
+.to("#project0",{
+    opacity: 100, 
+    ease: "power2.inOut",
+    
+}, 1)
     .from("#project1", { 
         y: "-10%",
         opacity: 0, 
@@ -40,7 +50,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 1)
+    }, 2)
     .from("#project2", { 
         y: "-10%",
         opacity: 0, 
@@ -49,7 +59,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 2) 
+    }, 3) 
     .from("#project3", { 
         y: "-10%",
         opacity: 0, 
@@ -58,7 +68,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 3)
+    }, 4)
     .from("#project4", { 
         y: "-10%",
         opacity: 0, 
@@ -67,7 +77,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 4) 
+    }, 5) 
     .from("#project5", { 
         y: "-10%",
         opacity: 0, 
@@ -76,7 +86,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 5) 
+    }, 6) 
     .from("#project6", { 
         y: "-10%",
         opacity: 0, 
@@ -85,7 +95,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 6)
+    }, 7)
     .from("#project7", { 
         y: "-10%",
         opacity: 0, 
@@ -94,7 +104,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 7)
+    }, 8)
     .from("#project8", { 
         y: "-10%",
         opacity: 0, 
@@ -103,7 +113,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 8)
+    }, 9)
 
     .from("#project9", { 
         y: "-10%",
@@ -113,7 +123,7 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 9)
+    }, 10)
 
     .from("#project10", { 
         y: "-10%",
@@ -123,5 +133,15 @@ timeline
         opacity: 100, 
         ease: "power2.inOut",
         
-    }, 10);
+    }, 11)
+    .from("#project11", { 
+        y: "-10%",
+        opacity: 0, 
+        delay: 1,
+    })
+    .to("#project11",{
+        opacity: 100, 
+        ease: "power2.inOut",
+        
+    }, 12);
 
